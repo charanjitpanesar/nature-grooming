29e873973791300a5201bcbe9ce2003690a0c348
Charanjit Singh <63778975+charanjitpanesar@users.noreply.github.com>
2025-09-26 17:29:04 +0000

    solve error

--- src/app/components/ProductBannerSection.js
+++ src/app/components/ProductBannerSection.js
@@ -11,13 +11,13 @@ const ProductBannerSection = () => {
                         <Col xxl={12} xl={12} lg={12} md={12} sm={12} xs={12}>
                             <div className='parent_area'>
                                 <div className='left_area'>
-                                    <h3>Men&apos;s Groom</h3>
+                                    <h3>Men Groom</h3>
                                 </div>
                                 <div className='right_area'>
                                     <ul>
                                         <li>Home <i className="bi bi-arrow-right"></i></li>
                                         <li>Shop <i className="bi bi-arrow-right"></i></li>
-                                        <li>Men&apos;s </li>
+                                        <li>Men </li>
                                     </ul>
                                 </div>
                             </div>
